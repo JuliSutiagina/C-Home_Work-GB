44a5510519e6676310f3a66c3e3299e0396eb6a0
JuliSutiagina <juli.sutiagina@yandex.ru>
2023-07-03 17:05:25 +0300

    Добавлена 6-я домашняя работа

---
+++ HW 6/Задача 41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write("Сколько цифр введете? ");
+int[] arr = new int[int.Parse(Console.ReadLine()!)]; //Создаем массив
+for (int i = 0; i< arr.Length; i++){
+    Console.Write($"{i+1} - ая: ");
+    arr[i] = int.Parse(Console.ReadLine()!); //Заполняем массив
+}
+
+Console.WriteLine($"[{String.Join(", ", arr)}] -> {MoreThenZero(arr)} из введенных вами больше 0");
+
+int MoreThenZero(int[] arr){
+    int count = 0;
+    foreach(int i in arr){
+        count += i>0 ? 1 : 0; //Проверка, больше ли элемент 0
+    }
+    return count;
+
+}
---
+++ HW 6/Задача 43/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите коэффициенты для уравнения первой прямой: b1, k1: ");
+Console.Write("b1 = ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("k1 = ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициенты для уравнения второй прямой: b2, k2: ");
+Console.Write("b2 =  ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.Write("k2 = ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double x1 = (b2 - b1)/(k1-k2); //Решаем совместно два уравнения и выражаем x1 - абсциссу пересечения двух прямых
+double y1 = k1*x1 + b1; // Подставляем найденное значение х в любое уравнение, чтобы получить ординату пересечения
+Console.WriteLine($"Точка пересечения двух прямых: ({x1}; {y1})");
+
