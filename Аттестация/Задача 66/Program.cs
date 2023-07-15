40c2dd0045a476d0ebad8bfa03d793ee0bbcfbd5
JuliSutiagina <juli.sutiagina@yandex.ru>
2023-07-15 22:46:14 +0300

    Добавлено решение задач промежуточной аттестации

---
+++ Аттестация/Задача 66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write($"Напилите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write($"Напилите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+if (M <= N){
+    Console.WriteLine($"{rekurs(M, N)}");
+}
+else{
+    Console.WriteLine($"M больше N. Поменяем M и N местами ");
+    Console.WriteLine($"{rekurs(N, M)}");
+}
+int rekurs(int M, int N){
+    if (M < N){
+        return N + rekurs(M, N-1);
+    }
+    if (M == N){
+        return M;
+    }
+    else{
+        return 0;
+    }
+}
---
+++ Аттестация/Задача 68/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. Выполнить с помощью рекурсии.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write($"Напилите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write($"Напилите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+
+
+
+if ((A < 0)|(B < 0))
+{
+    Console.WriteLine("Какое-то из введенных чисел меньше 0.");
+}
+else{
+    int result = Akkerman(A, B);
+    Console.WriteLine($"A({A},{B}) = {result}");
+}
+
+int Akkerman(int A, int B){
+    if ((A==0)&(B!=0)){
+        return B+1;
+    }
+    if ((A!=0)&(B==0)){
+        return Akkerman(A-1, 1);
+    }
+    else{
+        return Akkerman(A-1, Akkerman(A, B-1));
+    }
+}
